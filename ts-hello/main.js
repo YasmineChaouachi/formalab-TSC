83cf9be478578f4cbcb5c0d0090cb74e7d5dc75f
ChaouachiYasmine <chaouachiyasmine28@gmail.com>
2022-07-21 23:00:44 +0100

    Types

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,12 +1,47 @@
+/* --First TypeScript Program--*/
 // function log(message: string){
 //   console.log(message);
 // }
 // var message = 'hello world';
 // log(message);
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally :' + i);
-}
-doSomething();
+
+
+
+/*-------------------------*/
+
+
+
+/* --Declaring Variables--*/
+// function doSomething(){
+//   for (var i=0 ;i<5; i++){
+//     console.log(i);
+//   }
+//   console.log('Finally :' +i);
+// }
+// doSomething();
+
+
+
+/*-------------------------*/
+
+
+
+/* --Types--*/
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'k', false];
+var colorRed = 0;
+var colorGreen = 1;
+var colorBlue = 2;
+var Color;
+(function (Color) {
+  Color[Color["red"] = 0] = "red";
+  Color[Color["Green"] = 1] = "Green";
+  Color[Color["Blue"] = 2] = "Blue";
+  Color[Color["purple"] = 3] = "purple";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.red;
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,3 +1,4 @@
+/* --First TypeScript Program--*/
 // function log(message: string){
 //   console.log(message);
 // }
@@ -6,10 +7,38 @@
 
 // log(message);
 
-function doSomething(){
-  for (var i=0 ;i<5; i++){
-    console.log(i);
-  }
-  console.log('Finally :' +i);
-}
-doSomething();
+
+
+/*-------------------------*/
+
+
+
+/* --Declaring Variables--*/
+// function doSomething(){
+//   for (var i=0 ;i<5; i++){
+//     console.log(i);
+//   }
+//   console.log('Finally :' +i);
+// }
+// doSomething();
+
+
+
+/*-------------------------*/
+
+
+
+/* --Types--*/
+let a:number;
+let b:boolean;
+let c:string;
+let d:any;
+let e:number[]=[1,2,3];
+let f:any[]=[1,true,'k',false];
+
+const colorRed=0;
+const colorGreen=1;
+const colorBlue=2;
+
+enum Color{red=0,Green=1,Blue=2,purple=3};
+let backgroundColor=Color.red;
